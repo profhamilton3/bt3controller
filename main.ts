c835a60576fc57d3239074f0115327825608171c
T. Hamilton <profhamilton3@gmail.com>
2024-04-25 18:53:44 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
+    bluetooth.uartWriteNumber(3)
+})
 bluetooth.onBluetoothConnected(function () {
     basic.showLeds(`
         # . . . #
@@ -12,11 +15,17 @@ bluetooth.onBluetoothDisconnected(function () {
     joystickbit.Vibration_Motor(100)
     basic.showIcon(IconNames.Skull)
 })
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
+    bluetooth.uartWriteNumber(4)
+})
 input.onButtonPressed(Button.AB, function () {
     bluetooth.uartWriteNumber(0)
 })
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
+    bluetooth.uartWriteNumber(2)
+})
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
-	
+    bluetooth.uartWriteNumber(1)
 })
 joystickbit.initJoystickBit()
 bluetooth.startIOPinService()
